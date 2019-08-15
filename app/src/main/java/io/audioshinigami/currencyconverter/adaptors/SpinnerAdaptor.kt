404a4c2dfe9577e15c985328e28bb5937a669698
dave-oh-git <hirakoshinji722@gmail.com>
2019-08-15 16:04:57 +0100

    added adaptor for spinner (untested) :(

--- app/src/main/java/io/audioshinigami/currencyconverter/adaptors/SpinnerAdaptor.kt
+++ app/src/main/java/io/audioshinigami/currencyconverter/adaptors/SpinnerAdaptor.kt
@@ -1,24 +1,39 @@
 package io.audioshinigami.currencyconverter.adaptors
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import io.audioshinigami.currencyconverter.R
+import io.audioshinigami.currencyconverter.models.Currency
 
 class SpinnerAdaptor(): BaseAdapter() {
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    var data:ArrayList<Currency> = arrayListOf()
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+
+        val row = view ?: LayoutInflater.from(parent?.context).inflate(R.layout.spinner_item, parent, false)
+
+        val currency = data[position]
+
+        val imageView: ImageView = row.findViewById(R.id.id_spinner_image)
+        val textView: TextView = row.findViewById(R.id.id_spinner_text)
+        
+        textView.text = currency.name
+        imageView.setImageResource(currency.icon)
+
+        return row
     }
 
-    override fun getItem(p0: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItem(position: Int): Any {
+        return data[position]
     }
 
-    override fun getItemId(p0: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemId(p0: Int): Long = p0.toLong()
 
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
+    override fun getCount(): Int = data.size
 }
