7d8b068107d6bfa63dc33e1142cf5ebc6d3e32ec
dave-oh-git <hirakoshinji722@gmail.com>
2019-08-19 10:00:45 +0100

    action menu text colour & remove elevation from appbarlayout

--- app/src/main/java/io/audioshinigami/currencyconverter/HomeActivity.kt
+++ app/src/main/java/io/audioshinigami/currencyconverter/HomeActivity.kt
@@ -1,8 +1,10 @@
 package io.audioshinigami.currencyconverter
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import io.audioshinigami.currencyconverter.adaptors.SpinnerAdaptor
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,13 +12,21 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
         setUpSpinner()
     }
-
 
     private fun setUpSpinner(){
         val adaptor = SpinnerAdaptor()
         id_spinner_to.adapter = adaptor
         id_spinner_from.adapter = adaptor
     } /*end setupSpinner*/
-}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    } /*end onCreateOptions*/
+
+} /*end END*/
