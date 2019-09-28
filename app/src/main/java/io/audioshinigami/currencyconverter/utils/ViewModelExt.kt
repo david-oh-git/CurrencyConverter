4fe15e9b4e9a65724a9f6c3be349a6c7447be9f0
dave-oh-git <hirakoshinji722@gmail.com>
2019-09-28 15:01:04 +0100

    +++ update obtainViewModel wit CurrencySelectViewModel

--- app/src/main/java/io/audioshinigami/currencyconverter/utils/ViewModelExt.kt
+++ app/src/main/java/io/audioshinigami/currencyconverter/utils/ViewModelExt.kt
@@ -8,16 +8,23 @@ import io.audioshinigami.currencyconverter.convertAmount.CurrencyConvertViewMode
 import io.audioshinigami.currencyconverter.network.ApiFactory
 import io.audioshinigami.currencyconverter.repository.FlagDataRepository
 import io.audioshinigami.currencyconverter.repository.RateRepository
+import io.audioshinigami.currencyconverter.selectcurrency.CurrencySelectVMFactory
+import io.audioshinigami.currencyconverter.selectcurrency.CurrencySelectViewModel
 
 
 fun <T : ViewModel> Fragment.obtainViewModel(viewModelClass: Class<T>): T {
 
     return when{
-        viewModelClass.isInstance(CurrencyConvertViewModel() ) -> ViewModelProvider(this, setUpCurrencyConvertFactory()).get(viewModelClass)
+        viewModelClass.isAssignableFrom(CurrencyConvertViewModel::class.java) -> ViewModelProvider(this, setUpCurrencyConvertFactory()).get(viewModelClass)
+        viewModelClass.isAssignableFrom(CurrencySelectViewModel::class.java) -> ViewModelProvider(this, setupCurrencySelectFactory() ).get(viewModelClass)
         else -> ViewModelProvider(this).get(viewModelClass)
     }
 } // END obtainViewModel
 
 private fun setUpCurrencyConvertFactory(): CurrencyConvertVMFactory {
     return CurrencyConvertVMFactory(FlagDataRepository(), RateRepository(ApiFactory.rateApi))
+}
+
+private fun setupCurrencySelectFactory(): CurrencySelectVMFactory {
+    return CurrencySelectVMFactory( FlagDataRepository())
 }
