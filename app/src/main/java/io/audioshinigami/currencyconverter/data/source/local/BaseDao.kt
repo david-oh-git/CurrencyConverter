fc523ef69ad6406eb206b9c7205fa5e26db77134
david <hirakoshinji722@gmail.com>
2020-05-18 18:00:53 +0100

    add List function to BaseDao

--- app/src/main/java/io/audioshinigami/currencyconverter/data/source/local/BaseDao.kt
+++ app/src/main/java/io/audioshinigami/currencyconverter/data/source/local/BaseDao.kt
@@ -3,6 +3,7 @@ package io.audioshinigami.currencyconverter.data.source.local
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 /**
  * BaseDao for the app database
@@ -13,6 +14,9 @@ interface BaseDao<T> {
 
     @Insert
     fun add(obj: T)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addAll( data: List<T>)
 
     @Insert
     fun add(vararg objects: T)
