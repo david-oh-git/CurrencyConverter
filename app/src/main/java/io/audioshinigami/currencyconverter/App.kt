49939a5cceade0a192334990f44b80095e4d44c2
david <hirakoshinji722@gmail.com>
2020-07-19 10:03:38 +0100

    remove unused code in [App]

--- app/src/main/java/io/audioshinigami/currencyconverter/App.kt
+++ app/src/main/java/io/audioshinigami/currencyconverter/App.kt
@@ -41,16 +41,9 @@ class App: Application() {
         if( BuildConfig.DEBUG )
             Timber.plant( Timber.DebugTree() )
 
-        synchronized(this){
-            instance = this
-        }
     } /*end onCreate*/
 
     private fun initDagger(): AppComponent =
         DaggerAppComponent.factory().create(applicationContext)
 
-    companion object{
-        lateinit var instance: App
-        private set
-    }
 }
